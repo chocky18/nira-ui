271f2e8673d9506785231113298bdf1a1536a257
Naresh Tinnaluri <chockynaresh18@gmail.com>
2025-07-10 00:15:10 +0530

    removed launches in home

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
               <HealthConsultation />
               <BookAppointment />
               <HealthArticles />
-              <Launches />
+              {/* <Launches /> */}
               <FragranceSection />
               <CareSection />
               <CustomerReviews />
